369be7fc4c3d17096f4668e658daf1504238de7b
Leon <leonrubner2809@gmail.com>
2020-10-28 20:36:01 +0100

    conditional rendering react

---
+++ unreadMessages.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function MailBox(props) {
+    const unreadMessages = props.unreadMessages;
+    return (
+        <div>
+            <h1>Hello!</h1>
+            {unreadMessages.length > 0 &&
+                <h2>
+                    You have {unreadMessages.length} unread messages.
+                </h2>
+            }
+        </div>
+    );
+}
+
+const messages = ['React', 'Re: React', 'Re: Re: React'];
+
+ReactDOM.render(
+    <MailBox unreadMessages={messages} />,
+    document.getElementById('root')
+)
