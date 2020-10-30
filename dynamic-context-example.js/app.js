f508c404f91ec723ab0b017320b94e4a232173c0
Leon <leonrubner2809@gmail.com>
2020-10-31 00:09:04 +0100

    light-dark-mode lol

---
+++ dynamic-context-example.js/app.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {ThemeContext, themes} from "./theme-context";
+import ThemedButton from './themed-button';
+import ReactDOM from 'react-dom';
+
+function Toolbar(props) {
+    return (
+        <ThemedButton onClick={props.changeTheme}>
+            Change Theme
+        </ThemedButton>
+    );
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            theme: themes.light,
+        };
+
+        this.toggleTheme = () => {
+            this.setState(state => ({
+                theme:
+                    state.theme === themes.dark
+                        ? themes.light
+                        : themes.dark,
+            }));
+        };
+    }
+
+    render() {
+        return (
+            <Page>
+                <ThemeContext.Provider value={this.state.theme}>
+                    <Toolbar changeTheme={this.toggleTheme} />
+                </ThemeContext.Provider>
+                <Section>
+                    <ThemedButton />
+                </Section>
+            </Page>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
