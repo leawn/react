c759612b33d6db2737e2e8088a8905a7927bfc52
Leon <leonrubner2809@gmail.com>
2020-11-01 15:29:55 +0100

    started the hooks documentation

---
+++ hooks/effect-hook.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+function Example() {
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        document.title = `You clicked ${count} times`;
+    });
+
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+            </button>
+        </div>
+    )
+}
+
+ReactDOM.render()
