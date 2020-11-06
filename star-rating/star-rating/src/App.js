e1e85ea4173fd810b0bffee0cf77f138be243378
Leon <leonrubner2809@gmail.com>
2020-11-06 23:16:54 +0100

    something that was not commited before

--- star-rating/star-rating/src/App.js
+++ star-rating/star-rating/src/App.js
@@ -4,7 +4,8 @@ import StarRating from './starRating';
 
 function App() {
   return (
-      <StarRating />
+      <StarRating style={{ backgroundColor: 'lightblue'}}
+      />
   )
 }
 
--- star-rating/star-rating/src/starRating.js
+++ star-rating/star-rating/src/starRating.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import { FaStar } from 'react-icons/fa';
 import { Star, createArray } from './star-create-array';
 
-export default function StarRating( { totalStars = 5} ) {
+export default function StarRating( { style = {}, totalStars = 5, ...props} ) {
     const [selectedStars, setSelectedStars] = useState(0);
     return (
-        <>
+        <div style={{ padding: '5px', ...style}} {...props}>
             {createArray(totalStars).map((n, i) => (
                 <Star key={i}
                       selected={selectedStars > i}
@@ -15,6 +15,6 @@ export default function StarRating( { totalStars = 5} ) {
             <p>
                 {selectedStars} of {totalStars} stars
             </p>
-        </>
+        </div>
     )
 }
