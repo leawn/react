0ceaa98424e824d0c19cd7d88b9728c53bde197f
Leon <leonrubner2809@gmail.com>
2020-12-26 22:14:36 +0100

    im really in love with styled components wow

--- react-projects/styled-components-basic-project/src/App.js
+++ react-projects/styled-components-basic-project/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import styled from 'styled-components';
+
+import { Buttons, SearchBar } from './styles/elements';
+import searchIcon from './svg/search.svg';
+
+const Container = styled.div`
+  width: 1280px;
+  min-width: 1280px;
+  margin: 0 auto;
+  .row {
+    width: 100%;
+    display: flex;
+    align-content: center;
+  }
+`;
+
+
+const Subhead = styled.h3`
+  font-size: 24px;
+  font-weight: 600;
+  margin: 160px 0 48px;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+          <Subhead>Buttons</Subhead>
+          <div className="row">
+              <Buttons>Playlist</Buttons>
+              <Buttons outline>Playlist</Buttons>
+              <Buttons primary>Playlist</Buttons>
+              <Buttons secondary>Playlist</Buttons>
+          </div>
+          <Subhead>Search Bar</Subhead>
+          <div className="row">
+              <SearchBar squared>
+                  <img src={searchIcon} alt='search icon'/>
+                  <input placeholder='Search for podcasts, news, stories, etc.' type='text'/>
+              </SearchBar>
+          </div>
+      </Container>
     </div>
   );
 }
---
+++ react-projects/styled-components-basic-project/src/styles/global.js
@@ -0,0 +1,7 @@
+export const colors = {
+    white: '#fff',
+    black: '#000',
+    gray: '#818181',
+    primary: '#ffbf46',
+    secondary: '#59ffa0'
+};
