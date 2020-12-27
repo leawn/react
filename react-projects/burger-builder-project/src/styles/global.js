38b09cf02dd7c5e65e389f0d0a8549ec30bcc2d5
Leon <leonrubner2809@gmail.com>
2020-12-27 22:41:43 +0100

    initial commit

---
+++ react-projects/burger-builder-project/src/styles/global.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: 'Avenir', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+      Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
