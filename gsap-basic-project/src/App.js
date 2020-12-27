932f51caea2bddc8d6ca0e3cc9e24b14d9677535
Leon <leonrubner2809@gmail.com>
2020-12-27 12:17:49 +0100

    initial commit

--- gsap-basic-project/src/App.js
+++ gsap-basic-project/src/App.js
@@ -1,22 +1,58 @@
-import React, { useRef, useEffect } from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect, useState } from 'react';
 import './App.css';
 
 import { TweenMax, Power3 } from "gsap";
 
 function App() {
-  let logoItem = useRef(null);
-  let textItem = useRef(null);
+  let app = useRef(null);
+  let circle = useRef(null);
+  let circleRed = useRef(null);
+  let circleBlue = useRef(null);
+
+  const [state, setState] = useState(false);
+
+  const handleExpand = () => {
+    TweenMax.to(circleRed, .8, {width: 200, height: 200, ease: Power3.easeOut});
+    setState(true);
+  }
+
+  const handleShrink = () => {
+    TweenMax.to(circleRed, .8, {width: 75, height: 75, ease: Power3.easeOut});
+    setState(false);
+  }
 
   useEffect(() => {
-    TweenMax.to(logoItem, .8, {opacity: 1, y: -20, ease: Power3.easeOut});
-    TweenMax.to(textItem, .8, {opacity: 1, y: -20, ease: Power3.easeOut, delay: .2})
+    TweenMax.to(app, 0, {css: {visibility: 'visible'}});
+    /*TweenMax.from(circle, .8, {opacity: 0, x: 40, ease: Power3.easeOut});
+    TweenMax.from(circleRed, .8, {opacity: 0, x: 40, ease: Power3.easeOut, delay: .2});
+    TweenMax.from(circleBlue, .8, {opacity: 0, x: 40, ease: Power3.easeOut, delay: .4});*/
+    TweenMax.staggerFrom([circle, circleRed, circleBlue], .8, {opacity: 0, x: 40, ease: Power3.easeOut}, .2)
   }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
 
+  return (
+    <div
+        ref={el => {app = el}}
+        className="App"
+    >
+      <header className="App-header">
+        <div className='circle-container'>
+          <div
+              ref={el => {circle = el}}
+              className='circle'>
+          </div>
+
+          <div
+              onClick={state !== true ? handleExpand : handleShrink}
+              ref={el => {circleRed = el}}
+              className='circle red'>
+          </div>
+
+          <div
+              ref={el => {circleBlue = el}}
+              className='circle blue'>
+          </div>
+        </div>
       </header>
     </div>
   );
