2449a4ad5742f379467f1832b266911dfbe166ba
Leon <leonrubner2809@gmail.com>
2020-12-27 11:03:11 +0100

    initial commit

--- gsap-basic-project/src/App.js
+++ gsap-basic-project/src/App.js
@@ -1,22 +1,22 @@
+import React, { useRef, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { TweenMax, Power3 } from "gsap";
+
 function App() {
+  let logoItem = useRef(null);
+  let textItem = useRef(null);
+
+  useEffect(() => {
+    TweenMax.to(logoItem, .8, {opacity: 1, y: -20, ease: Power3.easeOut});
+    TweenMax.to(textItem, .8, {opacity: 1, y: -20, ease: Power3.easeOut, delay: .2})
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
