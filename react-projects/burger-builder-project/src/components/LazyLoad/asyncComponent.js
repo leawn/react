422c2c094487d2238a6194adb13c130489ad822c
Leon <leonrubner2809@gmail.com>
2021-01-02 22:42:58 +0100

    lazy load implementation

--- react-projects/burger-builder-project/src/components/LazyLoad/asyncComponent.js
+++ react-projects/burger-builder-project/src/components/LazyLoad/asyncComponent.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+//better to use:
+// const importComponent = React.lazy( () => import('./...'));
+// only for default exports tho
+
 const asyncComponent = (importComponent) => {
     return class extends Component {
         state = {
