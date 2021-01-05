91fc1f4c8948ad26fa23bb85421d126efd804e01
Leon <leonrubner2809@gmail.com>
2021-01-05 12:31:32 +0100

    hooks are cleaner than redux/rxjs. architecture rules

--- hooks/hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ hooks/hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -1,16 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
+const obj = {
+    id1 : {
+        title: 1,
+        amount: 2
+    },
+    id2 : {
+        title: 3,
+        amount: 3
+    }
+}
 import IngredientForm from './IngredientForm';
+import IngredientList from "./IngredientList";
 import Search from './Search';
 
 const Ingredients = () => {
+  const [ ingredients, setIngredients ] = useState([]);
+
+  useEffect(() => {
+      fetch('')
+          .then(res => res.json())
+          .then(resData => {
+              setIngredients(prevIngredients => [...prevIngredients, { _id: resData._id, ...JSON.parse(resData)}]);
+          })
+          .catch(err => {
+              console.log(err);
+          });
+  }, []);
+    // really depends on database we r using
+
+    const filteredIngredientsHandler = useCallback(filteredIngredients => {
+        setIngredients(filteredIngredients);
+    }, []);
+
+  const addIngredientHandler = ingredient => {
+      fetch('', {
+          method: 'POST',
+          body: JSON.stringify(ingredient),
+          headers: { 'Content-Type': 'application/json' }
+      })
+          .then(res => res.json())
+          .then(resData => {
+              setIngredients(prevIngredients => [...prevIngredients, { _id: resData._id, ...ingredient}]);
+          })
+          .catch(err => {
+              console.log(err);
+          });
+      // i would create some 'loading' state (true/false) where a component adds to the dom instantly
+      // but within we add a loader that shows that its waiting for a response, when we get the promise back
+      // with the response 201 then we remove that loader through changing state to false
+  };
+  const removeIngredientHandler = _id => {
+      setIngredients(prevIngredients => [...prevIngredients.filter(i => i._id !== _id)]);
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
-        {/* Need to add list here! */}
+        <Search onLoadIngredients={} />
+        <IngredientList ingredients={ingredients} onRemoveItem={removeIngredientHandler}/>
       </section>
     </div>
   );
--- hooks/hooks-01-starting-project/src/components/Ingredients/Search.js
+++ hooks/hooks-01-starting-project/src/components/Ingredients/Search.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+    const [ inputFilter, setInputFilter ] = useState('');
+
+    useEffect(() => {
+        fetch('')
+            .then(res => res.json())
+            .then(resData => {
+                set(prevIngredients => [...prevIngredients, { _id: resData._id, ...JSON.parse(resData)}]);
+                props.onLoadIngredients(loadedIngredients);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, [inputFilter]);
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input type="text" value={inputFilter} onChange={e => setInputFilter(e.target.value)}/>
         </div>
       </Card>
     </section>
