ed1e0381174605a4cdb5e7ab8be2c98bea1629c8
Leon <leonrubner2809@gmail.com>
2021-03-13 23:44:16 +0100

    initial commit

---
+++ react-projects/animated-menu-react-gsap/src/components/Hamburger.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const Hamburger = () => {
+    return <div className='hamburger-menu'>
+        <div className="menu-secondary-background-color"></div>
+        <div className="menu-layer">
+            <div className="menu-city-background">
+            </div>
+            <div className="container">
+                <div className="wrapper">
+                    <div className="menu-links">
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link to="/opportunities">Opportunities</Link>
+                                </li>
+                                <li>
+                                    <Link to="/solutions">Solutions</Link>
+                                </li>
+                                <li>
+                                    <Link to="/contact-us">Contact us</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>;
+};
+
+export default Hamburger;
