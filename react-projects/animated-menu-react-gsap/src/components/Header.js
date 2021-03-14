c08f44ba81eb80bc739125eeb696237404075bdb
Leon <leonrubner2809@gmail.com>
2021-03-14 22:34:46 +0100

    hamburger menu animations commit

--- react-projects/animated-menu-react-gsap/src/components/Header.js
+++ react-projects/animated-menu-react-gsap/src/components/Header.js
@@ -1,25 +1,79 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { withRouter, Link } from "react-router-dom";
 import Hamburger from "./Hamburger";
 
-const Header = () => {
-    return <header>
-        <div className="container">
-            <div className="wrapper">
-                <div className="inner-header">
-                    <div className="logo">
-                        <Link to="/">HMBRG.</Link>
-                    </div>
-                    <div className="menu">
-                        <button>
-                            Menu
-                        </button>
+const Header = ({ history }) => {
+
+    // State for menu button
+    const [state, setState] = useState({
+        initial: false,
+        clicked: null,
+        menuName: "Menu"
+    });
+
+    // State for disabled button
+    const [disabled, setDisabled] = useState(false);
+
+    // Use effect for page changes
+    useEffect(() => {
+        // listen for page changes
+        history.listen(() => {
+            setState({
+                clicked: false,
+                menuName: "Menu"
+            });
+        })
+    });
+
+    const handleMenu = () => {
+        disableMenu();
+        if (state.initial === false) {
+            setState({
+                initial: null,
+                clicked: true,
+                menuName: "Close"
+            });
+        } else if (state.clicked === true) {
+            setState({
+                clicked: !state.clicked,
+                menuName: "Menu"
+            });
+        } else if (state.clicked === false) {
+            setState({
+                clicked: !state.clicked,
+                menuName: "Close"
+            });
+        }
+    }
+
+    //Determine if our "Menu" button should be disabled
+    const disableMenu = () => {
+        setDisabled(!disabled);
+        setTimeout(() => {
+            setDisabled(false);
+        }, 1200);
+    }
+
+
+    return (
+        <header>
+            <div className="container">
+                <div className="wrapper">
+                    <div className="inner-header">
+                        <div className="logo">
+                            <Link to="/">HMBRG.</Link>
+                        </div>
+                        <div className="menu">
+                            <button disabled={disabled} onClick={handleMenu}>
+                                Menu
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <Hamburger />
-    </header>;
+            <Hamburger state={state}/>
+        </header>
+    )
 };
 
-export default Header;
+export default withRouter(Header);
